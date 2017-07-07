9e8124571a4d25c892300c2bb1681e0e3540b56a
newtonkiragu <karanunewton4@gmail.com>
2017-07-07 18:29:53 +0300

    made changes on my routing and fixed all errors

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
 require('sinatra')
 require('sinatra/reloader')
-require('descriptive_statistics')
-# require('./lib/word_count')
-# also_reload('lib/**/*.rb')
+also_reload('lib/**/*.rb')
+require('./lib/definition')
+require('./lib/word')
+require('pry')
 
 get('/') do
   erb(:index)
+end
+
+get('/words/new')do
+  erb(:words_form)
+end
+
+get('/words')do
+  @words = Word.all()
+  erb(:words)
+end
+
+post('/words')do
+  name = params.fetch('name')
+  Word.new(name).save()
+  @words = Word.all()
+  erb(:success)
+end
+
+get('/path_to_definitions/:id') do
+  @definition = definition.find(params.fetch('id').to_i())
+  erb(:words)
+end
+
+get('/def') do
+  erb(:definition)
+end
+
+get('/words/:id')do
+  id = params.fetch('id').to_i
+  @word = Word.find(id)
+
+  erb(:word)
+end
+
+get('/words/:id/definitions/new')do
+  id = params.fetch('id').to_i
+  @word = Word.find(id)
+  erb(:word_definition_form)
+end
+
+post('/definitions')do
+  name = params.fetch('name')
+  @definition = Definition.new({name:name})
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  binding.pry
+  erb(:success)
 end
